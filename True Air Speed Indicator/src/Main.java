bf0d772728d720567a7dccadb490b8ecb975535a
Kendall White <63067332+kendallw763@users.noreply.github.com>
2024-05-09 16:21:28 -0400

    Add files via upload

---
+++ True Air Speed Indicator/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        //take input from the user
+
+        //create a function that plugs in input into the TAS formula
+
+        // Create a function that displays the result
+
+        //create a message containing a string and user information
+    }
+}
---
+++ True Air Speed Indicator/src/Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Test {
+@Test
+    public void formulaTest(){
+
+    }
+}
