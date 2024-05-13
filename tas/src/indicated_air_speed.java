e36cf381889ceaf2decc5087be5b497959e8b46a
Kendall White <63067332+kendallw763@users.noreply.github.com>
2024-05-13 14:57:36 -0400

    Add files via upload

---
+++ tas/src/indicated_air_speed.java
@@ -0,0 +1,6 @@
+public class indicated_air_speed {
+    int altitude, ias;
+    dash dashBoard = new dash();
+
+// formula - TAS = IAS (1+ altitude x 0.00002)
+}
---
+++ tas/src/true_air_speed.java
@@ -0,0 +1,16 @@
+
+public class true_air_speed {
+    public static void main(String[] args) {
+int altitude, tas;
+dash dashBoard = new dash();
+
+        // formula of calculation - TAS = IAS (1+ altitude x 0.00002)
+        //call the dashboard object from the dash class to make it viewable in the main program...
+
+
+
+
+
+
+    }
+}
