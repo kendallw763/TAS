4ca2333ce92716d465313a6cad4c305bc26d7d83
Kendall White <63067332+kendallw763@users.noreply.github.com>
2024-05-14 07:13:52 -0400

    Add files via upload

---
+++ True Air Speed Indicator/src/indicated_air_speed.java
@@ -0,0 +1,13 @@
+
+public class indicated_air_speed extends true_air_speed {
+    int altitude, indicated_speed;
+//turn the functions of the IAS and pitot into code
+
+
+
+
+
+
+
+
+}
---
+++ True Air Speed Indicator/src/true_air_speed.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class true_air_speed {
+    Scanner input = new Scanner(System.in);//prompt user to enter aircraft information.
+    //prompt user to enter:
+    // commercial
+    //is the aircraft two man or one?
+    //max speed
+    //max altitude
+
+
+    //take the user input and calculate the true airspeed of the aircraft
+}
